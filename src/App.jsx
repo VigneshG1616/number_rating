ec362e508607b2a9882f0de70d22a18ad6f87017
VigneshG1616 <vg.thefire@gmail.com>
2023-08-18 04:13:35 +0000

    added useCallback and passing maxrating as props

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 export default function App() {
   return (
     <>
-    <main>
-      <h1 style={{height:"10px",fontFamily:"calibri"}}>Rate Me..!</h1>
-    </main>
-    <NumberRating />
-  </>
-      )
+      <main>
+        <h1 style={{ height: "10px", fontFamily: "calibri" }}>Rate Me..!</h1>
+      </main>
+      <NumberRating maxRating={5} />
+    </>
+  )
 }
--- src/components/number_rating_component.jsx
+++ src/components/number_rating_component.jsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./number_rating_component_style.css";
 
-const NumberRating = () => {
-  let ratingNumber = 5;
+const NumberRating = ({ maxRating = 10 }) => {
+
   const [currentRating, setCurrentRating] = useState(0);
   const [hoverValue, setHoverValue] = useState(0);
-  
-  const currentRatingHandler = (getRatingValue) => {
-    getRatingValue == currentRating ? setCurrentRating(0) : setCurrentRating(getRatingValue);
-  }
 
+  const currentRatingHandler = useCallback((getRatingValue) => {
+    getRatingValue == currentRating ? setCurrentRating(0) : setCurrentRating(getRatingValue);
+  }, [currentRating]);
   return (
 
     <div className="number-rating-container">{
-      [...Array(ratingNumber)].map((_, idx) => {
+      [...Array(maxRating)].map((_, idx) => {
         const ratingValue = idx + 1;
         return <h2
           className={`number-rating ${ratingValue <= (hoverValue || currentRating) ? 'active' : ''}`}
           key={idx}
           onClick={() => currentRatingHandler(ratingValue)}
-          onMouseEnter={()=>setHoverValue(ratingValue)}
-          onMouseLeave={()=>setHoverValue(0)}>
+          onMouseEnter={() => setHoverValue(ratingValue)}
+          onMouseLeave={() => setHoverValue(0)}>
           {ratingValue}
         </h2>
       }
